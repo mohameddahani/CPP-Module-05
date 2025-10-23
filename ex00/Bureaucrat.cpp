000d3f9052a0763b299fc538e6941dade6ded801
Mohamed Dahani <mohamedxdahani@gmail.com>
2025-10-23 17:00:17 +0100

    fix some problems in logic

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 18:15:56 by mdahani           #+#    #+#             */
-/*   Updated: 2025/10/23 11:25:39 by mdahani          ###   ########.fr       */
+/*   Updated: 2025/10/23 15:49:25 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,9 +43,9 @@ Bureaucrat::Bureaucrat(): name("unknown name"), grade(150){
 // * Parametrised constructor
 Bureaucrat::Bureaucrat(const std::string &name, const unsigned int &grade): name(name){
     if (grade < 1){
-        throw Bureaucrat::GradeTooLowException();
-    } else if (grade > 150){
         throw Bureaucrat::GradeTooHighException();
+    } else if (grade > 150){
+        throw Bureaucrat::GradeTooLowException();
     } else {
         this->grade = grade;
     }
@@ -82,29 +82,29 @@ unsigned int Bureaucrat::getGrade() const{
 
 // * Methods
 void Bureaucrat::incrementBureaucrat(){
-    if (this->grade > 1 && this->grade <= 150){
-        this->grade--;
-    } else {
-        throw Bureaucrat::GradeTooLowException();
+    if (this->grade <= 1){
+        throw Bureaucrat::GradeTooHighException();
     }
+
+    this->grade--;
 }
 
 void Bureaucrat::decrementBureaucrat(){
-    if (this->grade >= 1 && this->grade < 150){
-        this->grade++;
-    } else {
-        throw Bureaucrat::GradeTooHighException();
+    if (this->grade >= 150){
+        throw Bureaucrat::GradeTooLowException();
     }
+
+    this->grade++;
 }
 
 // * Nested Class
 // ! _GLIBCXX_NOTHROW: is mean (noexcept) will not throw
 const char *Bureaucrat::GradeTooHighException::what() const _GLIBCXX_NOTHROW {
-    return "Exception: The Grade of Bureaucrat is Higher than 150";
+    return "Bureaucrat grade is too high!";
 }
 
 const char *Bureaucrat::GradeTooLowException::what() const _GLIBCXX_NOTHROW {
-    return "Exception: The Grade of Bureaucrat is Lower than 1";
+    return "Bureaucrat grade is too low!";
 }
 
 // * Insertion operator
